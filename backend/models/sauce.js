dc7bebea8ec0cefbd4580b3329bd9ac0bdbcd52e
YungDavo <davidcbne@gmail.com>
2021-09-02 17:23:32 +0200

    Sauce validation

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -5,10 +5,15 @@ const sanitizerPlugin = require("mongoose-sanitizer-plugin");
 
 const sauceSchema = mongoose.Schema({
   userId: { type: String, required: true },
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
+  name: {
+    type: String,
+    required: true,
+    minLength: [3, "Must be at least 3 characters, got {VALUE}"],
+    maxLength: [30, "Must be less than 50 characters, got {VALUE}"],
+  },
+  manufacturer: { type: String, required: true, minLength: 3, maxLength: 30 },
+  description: { type: String, required: true, minLength: 3, maxLength: 50 },
+  mainPepper: { type: String, required: true, minLength: 3, maxLength: 20 },
   imageUrl: { type: String, required: true },
   heat: { type: Number, required: true },
   likes: { type: Number },
